a8ae3cae9eca5d8012db4473af2b176764548d8f
anxueyu <1358042645@qq.com>
2026-03-04 13:34:15 +0800

    feat: 重置预览界面

--- src/components/code-preview.vue
+++ src/components/code-preview.vue
@@ -1,45 +1,19 @@
 <script setup lang="ts">
-import { defineProps, defineEmits, computed } from 'vue'
-import hljs from 'highlight.js'
-import 'highlight.js/styles/default.css'
-
+import { defineProps } from "vue";
 // 定义组件props
 interface Props {
-  code: string          // 接收父组件传入的代码
-  language: string      // 代码语言类型（如 'javascript', 'python', 'vue' 等）
+  code: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  language: 'vue'       // 默认语言类型
-})
-
-const emit = defineEmits(['update:code'])
-
-// 计算高亮后的代码
-const highlightedCode = computed(() => {
-  return hljs.highlight(props.code, { language: props.language }).value
-})
-
-// 处理内容变化事件
-// 修改事件处理函数参数类型
-const preChange = (e: Event) => {
-  // 类型断言确保目标是HTMLElement
-  const target = e.target as HTMLElement
-  const newCode = target.innerText
-  emit('update:code', newCode)
-}
-
-// 监听代码变化并更新高亮（计算属性已自动处理）
+  code: "vue",
+});
 </script>
 
 <template>
-  <pre 
-    @input="preChange" 
-    contenteditable="true" 
-    v-html="highlightedCode" 
-    class="tool-code hljs"
-    :class="`hljs ${language}`" 
-  ></pre>
+  <pre class="tool-code hljs"
+    >{{ props.code }}
+  </pre>
 </template>
 
 <style scoped lang="scss">
@@ -47,9 +21,7 @@ const preChange = (e: Event) => {
   padding: 0;
   margin: 0;
   overflow: auto;
-  white-space: pre;
-  background: #ffffff8c;
-  color: #0084ff;
+  color:rgb(142, 200, 255);
   height: 100%;
 }
 </style>
