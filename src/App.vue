767cd0387c7d044b99252ed7199b4d94a0e51669
anxueyu <1358042645@qq.com>
2026-03-06 13:20:48 +0800

    feat: 进一步优化样式；增加github仓库链接

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ const desc1 = ref(`
   `);
 const desc2 = ref(`
   title="弹窗居中" 
-  left="calc(50% - 100px)"
+  left="calc(50% - 200px)"
   `);
 const desc3 = ref(`
   title="从右侧弹出" 
@@ -28,13 +28,13 @@ const desc4 = ref(`
   :top="300" 
   :left="20" 
   @closed="handleFold" 
-  :thumbnail="{icon: 'vueIcon'}"
+  :thumbnail="{icon: vueIcon}"
   `);
 const desc5 = ref(`
   title="带ICON弹窗" 
   :top="300" 
-  icon='vueIcon' 
-  left="calc(50% - 100px)" 
+  :icon="vueIcon" 
+  left="calc(50% - 200px)" 
   `);
 const desc6 = ref(`
   title="没有头部" 
@@ -46,18 +46,14 @@ const desc7 = ref(`
   :top="600" 
   :left="20" 
   <template #title>
-    <div class="text-sub">
-      自定义头部
-    </div>
+    ...
   </template>`);
 const desc8 = ref(` 
   title="footer自定义" 
   :top="600" 
-  left="calc(50% - 100px)" 
+  left="calc(50% - 200px)" 
   <template #footer>
-    <div class="footer">
     ...
-    </div>
   </template>
   `);
 
@@ -69,28 +65,73 @@ const handleFold = () => {
 
 <template>
   <div class="context">
-    <div :class="!showDialog1 ? 'btn btn-plain' : 'btn'" @click="showDialog1 = !showDialog1">
+    <div class="github-link">
+      <a
+        href="https://github.com/whanxueyu/vue-free-dialog"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <svg
+          height="24"
+          width="24"
+          viewBox="0 0 16 16"
+          version="1.1"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+          ></path>
+        </svg>
+        GitHub Repository
+      </a>
+    </div>
+    <div
+      :class="!showDialog1 ? 'btn btn-plain' : 'btn'"
+      @click="showDialog1 = !showDialog1"
+    >
       弹窗左
     </div>
-    <div :class="!showDialog2 ? 'btn btn-plain' : 'btn'" @click="showDialog2 = !showDialog2">
+    <div
+      :class="!showDialog2 ? 'btn btn-plain' : 'btn'"
+      @click="showDialog2 = !showDialog2"
+    >
       弹窗中
     </div>
-    <div :class="!showDialog3 ? 'btn btn-plain' : 'btn'" @click="showDialog3 = !showDialog3">
+    <div
+      :class="!showDialog3 ? 'btn btn-plain' : 'btn'"
+      @click="showDialog3 = !showDialog3"
+    >
       弹窗右
     </div>
-    <div :class="!showDialog4 ? 'btn btn-plain' : 'btn'" @click="showDialog4 = !showDialog4">
+    <div
+      :class="!showDialog4 ? 'btn btn-plain' : 'btn'"
+      @click="showDialog4 = !showDialog4"
+    >
       可折叠弹窗
     </div>
-    <div :class="!showDialog5 ? 'btn btn-plain' : 'btn'" @click="showDialog5 = !showDialog5">
+    <div
+      :class="!showDialog5 ? 'btn btn-plain' : 'btn'"
+      @click="showDialog5 = !showDialog5"
+    >
       带 ICON 弹窗
     </div>
-    <div :class="!showDialog6 ? 'btn btn-plain' : 'btn'" @click="showDialog6 = !showDialog6">
+    <div
+      :class="!showDialog6 ? 'btn btn-plain' : 'btn'"
+      @click="showDialog6 = !showDialog6"
+    >
       没有头部
     </div>
-    <div :class="!showDialog7 ? 'btn btn-plain' : 'btn'" @click="showDialog7 = !showDialog7">
+    <div
+      :class="!showDialog7 ? 'btn btn-plain' : 'btn'"
+      @click="showDialog7 = !showDialog7"
+    >
       头部自定义
     </div>
-    <div :class="!showDialog8 ? 'btn btn-plain' : 'btn'" @click="showDialog8 = !showDialog8">
+    <div
+      :class="!showDialog8 ? 'btn btn-plain' : 'btn'"
+      @click="showDialog8 = !showDialog8"
+    >
       底部自定义
     </div>
     <div class="tips">
@@ -112,7 +153,7 @@ const handleFold = () => {
     title="弹窗居中"
     :width="400"
     height="280"
-    left="calc(50% - 100px)"
+    left="calc(50% - 200px)"
     @closed="showDialog2 = false"
   >
     <CodePreview v-model:code="desc2" language="html"></CodePreview>
@@ -147,7 +188,7 @@ const handleFold = () => {
     :icon="vueIcon"
     :width="400"
     height="280"
-    left="calc(50% - 100px)"
+    left="calc(50% - 200px)"
     @closed="showDialog5 = false"
   >
     <CodePreview v-model:code="desc5" language="html"></CodePreview>
@@ -200,14 +241,12 @@ const handleFold = () => {
     :top="600"
     :width="400"
     height="280"
-    left="calc(50% - 100px)"
+    left="calc(50% - 200px)"
     @closed="showDialog8 = false"
   >
     <template #footer>
       <div class="footer">
-        <button class="btn" @click="showDialog8 = false">
-          确定
-        </button>
+        <button class="btn" @click="showDialog8 = false">确定</button>
         <button class="btn btn-plain" @click="showDialog8 = false">取消</button>
       </div>
     </template>
@@ -220,10 +259,28 @@ const handleFold = () => {
   bottom: 0;
   position: absolute;
   margin: 20px;
+  .github-link {
+    display: inline-block;
+    padding: 6px 12px;
+    background-color: #a340ff;
+    border-radius: 6px;
+    margin-right: 10px;
+
+    a {
+      color: #ffffff;
+      text-decoration: none;
+      font-size: 14px;
+      font-weight: 500;
+      transition: all 0.2s ease;
+    }
+    &:hover {
+      background-color: #3445a3;
+    }
+  }
   .tips {
-    margin-top: 20px;
-    background-color: #7c7c7c99;
-    padding: 10px 20px;
+    margin-top: 10px;
+    background-color: #27447099;
+    padding: 6px 10px;
   }
 }
 .slot-title {
@@ -259,7 +316,6 @@ const handleFold = () => {
   margin-right: 10px;
   background-color: #409eff;
   color: white;
-
 
   &.btn-plain {
     background-color: transparent;
